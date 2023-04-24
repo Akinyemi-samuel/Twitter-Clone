fd48bb13966c3c73198e19b81c9fb7abc4b986df
samuel <samfrosh673@gmail.com>
2023-04-24 21:03:36 +0100

    updated the home page design by creating the fixed navigation

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  options = 'for you';
 
+  toggleoptions(value: string) {
+    this.options = value;
+  }
 }
