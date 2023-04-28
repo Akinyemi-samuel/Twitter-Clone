60b813b6cbc08b4f38425752fe3cfea887d3dbb4
samuel <samfrosh673@gmail.com>
2023-04-28 13:53:39 +0100

    updated the trends section ... by creating the template for each trends Changes to be committed: 	modified:   .hintrc 	modified:   frontend/src/app/components/home/home.component.scss 	modified:   frontend/src/app/components/navigation/navigation.component.html 	modified:   frontend/src/app/components/navigation/navigation.component.scss 	modified:   frontend/src/app/components/trends/trends.component.html 	modified:   frontend/src/app/components/trends/trends.component.scss 	modified:   frontend/src/app/components/trends/trends.component.ts 	modified:   frontend/src/index.html 	modified:   frontend/src/styles.scss

--- frontend/src/app/components/trends/trends.component.ts
+++ frontend/src/app/components/trends/trends.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class TrendsComponent {
 
+
+   products: Product[] = [
+    { location: 'Trends in Nigeria', topic: 'Brother Benard', amount:17.4 },
+    { location: 'Trends in Nigeria', topic: '#GirlsinICT', amount:8.53 },
+    { location: 'Trends in Nigeria', topic: 'NEWCASTLE WELCOMES KHOSI', amount:17.4 },
+    { location: 'Trends in Nigeria', topic: 'Airport', amount:111.4 },
+    { location: 'Trends in Nigeria', topic: 'Effie', amount:100.4 },
+    { location: 'Trends in Nigeria', topic: 'Deborah', amount:660.4 },
+    { location: 'Trends in Nigeria', topic: 'Deborah', amount:660.4 },
+    { location: 'Trends in Nigeria', topic: 'Deborah', amount:660.4 },
+    { location: 'Trends in Nigeria', topic: 'Deborah', amount:660.4 },
+    
+  ];
+  
+}
+
+interface Product {
+  location: string;
+  topic: string;
+  amount: number;
 }
