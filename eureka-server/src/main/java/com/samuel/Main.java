3c423c478cb69f942594a3edd11f23d85ae5bcfa
samuel <samfrosh673@gmail.com>
2023-05-16 13:37:18 +0100

    updated the eureka-server module Main class

--- eureka-server/src/main/java/com/samuel/Main.java
+++ eureka-server/src/main/java/com/samuel/Main.java
@@ -1,7 +1,11 @@
 package com.samuel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
