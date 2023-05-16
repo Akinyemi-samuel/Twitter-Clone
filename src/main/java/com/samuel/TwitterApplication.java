ae2de9b4e19307736d8383434c9cbe536d4cb6ef
samuel <samfrosh673@gmail.com>
2023-05-16 13:16:03 +0100

    updated the parent repository(twitter clone) files

---
+++ src/main/java/com/samuel/TwitterApplication.java
@@ -0,0 +1,13 @@
+package com.samuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuel/TwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
