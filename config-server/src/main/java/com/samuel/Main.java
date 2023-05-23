c17ccbfac68e7eb79fb62e5759d1c68b54db560e
samuel <samfrosh673@gmail.com>
2023-05-23 09:24:46 +0100

    wrote the configurations for eureka-server and connected it to the config server

--- config-server/src/main/java/com/samuel/Main.java
+++ config-server/src/main/java/com/samuel/Main.java
@@ -2,8 +2,12 @@ package com.samuel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- eureka-server/src/main/java/com/samuel/Main.java
+++ eureka-server/src/main/java/com/samuel/Main.java
@@ -2,7 +2,11 @@ package com.samuel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
