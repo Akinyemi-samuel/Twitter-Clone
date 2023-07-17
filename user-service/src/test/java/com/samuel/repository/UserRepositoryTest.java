e259559af422c0eaa3e9f07f1f5135f48781943d
samuel <akinyemisamuelayo@gmail.com>
2023-07-17 21:08:21 +0100

    Testing user-service

---
+++ user-service/src/test/java/com/samuel/repository/UserRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.samuel.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void updatePasswordById() {
+    }
+}
