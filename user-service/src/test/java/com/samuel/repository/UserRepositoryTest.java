b02f01adfb1c2e7de3d17c43d25f0214c0eb9fff
samuel <akinyemisamuelayo@gmail.com>
2023-07-20 18:42:33 +0100

    configuring the testing environment properties

--- user-service/src/test/java/com/samuel/repository/UserRepositoryTest.java
+++ user-service/src/test/java/com/samuel/repository/UserRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.samuel.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@DataJpaTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class UserRepositoryTest {
 
     @Test
