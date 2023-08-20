24b7403388c96fd37fca8db931fc88537b0a3a67
samuel <akinyemisamuelayo@gmail.com>
2023-08-20 22:54:19 +0100

    code base clean-up

--- user-service/src/main/java/com/samuel/controller/AuthenticationController.java
+++ user-service/src/main/java/com/samuel/controller/AuthenticationController.java
@@ -3,9 +3,11 @@ package com.samuel.controller;
 import com.samuel.dto.request.AuthenticationRequest;
 import com.samuel.dto.response.AuthenticationResponse;
 import com.samuel.service.AuthenticationService;
-import com.samuel.util.AuthenticationUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
