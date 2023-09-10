ed5837767514108a06f0e202b707b8b73e1f37e9
samuel <akinyemisamuelayo@gmail.com>
2023-09-10 17:16:05 +0100

    created the tweet service module and configured its depedencies

---
+++ tweet-service/src/test/java/com/example/tweetservcie/TweetServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tweetservcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetServcieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
