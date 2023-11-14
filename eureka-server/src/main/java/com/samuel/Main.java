6182dbaf6cd71baa53170b3d76301c2be7c3d680
samuel <akinyemisamuelayo@gmail.com>
2023-11-14 09:03:54 +0100

    creating tweet service and created environment variable for all service in  docker-compose.yml

--- eureka-server/src/main/java/com/samuel/Main.java
+++ eureka-server/src/main/java/com/samuel/Main.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableDiscoveryClient
 @EnableEurekaServer
-//@EnableDiscoveryClient
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
