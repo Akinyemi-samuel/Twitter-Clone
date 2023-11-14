b58304e1fde9f8208282998e661e2c711f48cafc
samuel <akinyemisamuelayo@gmail.com>
2023-11-14 14:27:08 +0100

    creating tweet service and created environment variable for all service in  docker-compose.yml

--- config-server/src/main/java/com/samuel/Main.java
+++ config-server/src/main/java/com/samuel/Main.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
